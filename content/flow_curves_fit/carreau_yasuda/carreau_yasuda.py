496b3b8fe925f731add715cb1abb1a129a202b7b
Rajas Shah <rajas.s.shah@gmail.com>
2023-10-15 19:46:10 -0600

    Add carreau yasuda model and notebook

--- content/flow_curves_fit/carreau_yasuda/carreau_yasuda.py
+++ content/flow_curves_fit/carreau_yasuda/carreau_yasuda.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 
-def make_eta_star(df):
+def calc_eta_star(df):
     """Calculates complex viscosity
     """
     df['eta_star'] = (np.sqrt(np.power(df['G_p'], 2) + np.power(df['G_pp'], 2))
@@ -20,10 +20,10 @@ def model(w, eta_0, eta_inf, a, n, lam):
         Parameters:
         w = Angular frequency
         eta_0 = zero-shear viscosity
-        eta_ing = infinite-shear viscosity
+        eta_inf = infinite-shear viscosity
+        lam = relaxation time
         a = transition parameter
         n = power law index
-        lam = relaxation time
     """
     eta_star = eta_inf + (eta_0-eta_inf)/((1+(w*lam)**a)**((1-n)/a))
     eta_star = eta_star.rename('eta_star')
@@ -36,19 +36,20 @@ def fit(df, eta_0=None, eta_inf=1E-4, lam=0.5, a=0.5, n=0.5):
         Parameters:
         df = DataFrame containing LVE data
         eta_0 = zero-shear viscosity
-        eta_ing = infinite-shear viscosity
+        eta_inf = infinite-shear viscosity
+        lam = relaxation time
         a = transition parameter
         n = power law index
-        lam = relaxation time
     """
 
-    if not 'eta_star' in df.columns:
-        df = make_eta_star(df)
+    if 'eta_star' not in df.columns:
+        df = calc_eta_star(df)
     if eta_0 is None:
         eta_0 = df.iloc[0]['eta_star']
 
     popt, *pcov = curve_fit(model, df['w'], df['eta_star'],
                             p0=[eta_0, eta_inf, a, n, lam],
-                            bounds=(0, np.inf), loss='soft_l1', f_scale=0.1)
+                            bounds=(0, np.inf),
+                            loss='soft_l1', f_scale=0.1)
     out_df = pd.concat([df['w'], model(df['w'], *popt)], axis=1)
     return (out_df, popt)
